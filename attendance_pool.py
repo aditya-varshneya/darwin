66bde6c13e64bb757d175f0cbd1da694ba21e8e2
aditya-varshneya <59046585+aditya-varshneya@users.noreply.github.com>
2024-02-21 20:22:55 +0530

    commit max_validation

--- attendance_pool.py
+++ attendance_pool.py
@@ -10,9 +10,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-url ='https://thbhrms.darwinbox.in/user/login'
+url = 'https://thbhrms.darwinbox.in/user/login'
 
-class Testlogin( ):
+
+class Testlogin():
     driver = None
 
     def __init__(self):
@@ -20,12 +21,12 @@ class Testlogin( ):
 
     @classmethod
     def test_setup(self, url):
-        chrome_options = webdriver.ChromeOptions( )
+        chrome_options = webdriver.ChromeOptions()
         prefs = {"profile.default_content_setting_values.notifications": 1}
         chrome_options.add_experimental_option("prefs", prefs)
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(2)
-        self.driver.maximize_window( )
+        self.driver.maximize_window()
         self.driver.get(url)
         time.sleep(2)
         return self.driver
@@ -34,30 +35,34 @@ class Testlogin( ):
         # __doc__="Test discription"
         driver = self.driver
         time.sleep(5)
-        driver.find_element(By.ID, "UserLogin_username").click( )
+        driver.find_element(By.ID, "UserLogin_username").click()
         driver.find_element(By.ID, "UserLogin_username").send_keys(username)
         time.sleep(3)
-        driver.find_element(By.ID, "UserLogin_password").click( )
+        driver.find_element(By.ID, "UserLogin_password").click()
         driver.find_element(By.ID, "UserLogin_password").send_keys(password)
         time.sleep(3)
-        driver.find_element(By.ID, "login-submit").click( )
+        driver.find_element(By.ID, "login-submit").click()
 
 
 class attendance():
     def clock_in(self):
         driver = Testlogin().driver
-        driver.find_element(By.XPATH,"/html/body/div[2]/div/nav/div[1]/div/div[8]/div").click()
+        driver.find_element(By.XPATH, "/html/body/div[2]/div/nav/div[1]/div/div[8]/div").click()
         time.sleep(5)
-        driver.find_element(By.XPATH,"//ul[@class='list-inline display-inline-flex align-items-center']//a[@id='attendance-logger-widget']").click()
+        contextshadow = driver.find_element(By.CSS_SELECTOR, '[id="dbox-top-bar"]').shadow_root
+        time.sleep(2)
+        shadow_text = contextshadow.find_element(By.CSS_SELECTOR,".mr-8.db_btn_clock.tooltip_clock")
+        shadow_text.click()
         time.sleep(10)
         driver.close()
 
-
     def clock_out(self):
-        driver = Testlogin( ).driver
-        driver.find_element(By.XPATH, "/html/body/div[2]/div/nav/div[1]/div/div[8]/div").click( )
+        driver = Testlogin().driver
+        driver.find_element(By.XPATH, "/html/body/div[2]/div/nav/div[1]/div/div[8]/div").click()
         time.sleep(5)
-        driver.find_element(By.XPATH,
-                            "//ul[@class='list-inline display-inline-flex align-items-center']//a[@id='attendance-logger-widget']").click( )
+        contextshadow = driver.find_element(By.CSS_SELECTOR, '[id="dbox-top-bar"]').shadow_root
+        time.sleep(2)
+        shadow_text = contextshadow.find_element(By.CSS_SELECTOR, ".mr-8.db_btn_clock.tooltip_clock")
+        shadow_text.click()
         time.sleep(10)
-        driver.close( )
+        driver.close()
